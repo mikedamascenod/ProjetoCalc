897d28c366c4b04341c0c7009631321d02710579
michel moreira damasceno <damasceno.michel@outlook.com>
2020-07-30 20:42:13 -0300

    Calculadora feita em python

---
+++ Calc.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite outro numero: '))
+Soma = n1 + n2
+Subtração = n1 - n2
+multiplicação = n1 * n2
+divisão = n1 / n2
+print('A Soma entre o {} e {} resulta em {:.2f}'.format(n1, n2, Soma))
+print('A Subtração entre {} e {} resulta em {:.2f}'.format(n1, n2, Subtração))
+print('A multiplicação entre {} e {} resulta em {:.2f}'.format(n1, n2, multiplicação))
+print('A divisão entre {} e {} resulta em {:.2f}'.format(n1, n2, divisão))
